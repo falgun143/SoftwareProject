8af92ed966e1cacd72b565090b23dda188324cb5
falgun143 <palfalgun@gmail.com>
2023-11-13 16:35:02 +0530

    Changes Done

---
+++ packages/ui/Login.tsx
@@ -0,0 +1,12 @@
+
+
+const Login = () => {
+  return (
+    <div>   
+        <input type="text" placeholder="Enter your username"  />
+        <input type="password" placeholder="Enter your password"   />
+    </div>
+  )
+}
+
+export default Login
